b4b66551e3ff9884d9872600ce00e94a5a8716e1
smorley <smorley@lanl.gov>
2020-09-10 11:45:38 -0600

    util: update summary plotter to do AU/AL instead of AE

--- util.py
+++ util.py
@@ -50,11 +50,26 @@ def results_summary(log, geolog, show=True):
     """3-panel summary plot from log and geoindex files
     """
     fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
-    geolog.add_ae_quicklook(plot_obs=True, target=axes[0])
-    geolog.add_kp_quicklook(plot_obs=True, target=axes[1])
-    log.add_dst_quicklook(plot_obs=True, target=axes[2])
+    okw = {'c':'purple', 'ls':'--', 'lw':1.5}
+    mkw = {'c':'C0', 'ls':'-', 'lw':1.5}
+    geolog.add_ae_quicklook(plot_obs=True, target=axes[0], val='AU', obs_kwargs=okw,
+                            add_legend=False, **mkw)
+    okw = {'c':'k', 'ls':'--', 'lw':1.5}
+    mkw = {'c':'C1', 'ls':'-', 'lw':1.5}
+    geolog.add_ae_quicklook(plot_obs=True, target=axes[0], val='AL', obs_kwargs=okw,
+                            add_legend=False, **mkw)
+    geolog.add_kp_quicklook(plot_obs=True, target=axes[1], add_legend=False)
+    log.add_dst_quicklook(plot_obs=True, target=axes[2], add_legend=False)
     axes[0].set_xlabel('')
     axes[1].set_xlabel('')
+    axes[0].set_ylabel('AU/AL [nT]')
+    axes[2].set_ylabel('Dst [nT]')
+    from matplotlib.lines import Line2D
+
+    legend_elements = [Line2D([0], [0], color='k', lw=1.5, label='Virtual (solid)'),
+                       Line2D([0], [0], color='k', lw=1.5, ls='--', label='Observed (dashed)'),
+                       ]
+    axes[2].legend(handles=legend_elements, loc='lower left', ncol=2)
     if show:
         plt.show()
     return fig, axes
